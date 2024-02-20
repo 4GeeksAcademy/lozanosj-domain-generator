4393e7a71d2441bfee1c689291f0f26acfdd966e
Jhonny Lozano <110267545+lozanosj@users.noreply.github.com>
2024-02-20 01:05:00 +0000

    Proyecto terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".edu", ".ve", ".net"];
+
+  let domainGenerator = function(pronoun, adj, noun, domain) {
+    let website = [];
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < domain.length; l++) {
+            website.push(pronoun[i] + adj[j] + noun[k] + domain[l]);
+          }
+        }
+      }
+    }
+    console.log(website);
+  };
+  domainGenerator(pronoun, adj, noun, domain);
 };
+/* eslint-enable no-console */
